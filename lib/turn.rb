b24f3b2db6b0d3ececdfd9924aee5744aa3f0a0d
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-01 02:44:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,6 +24,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i
+end
 
 #valid_move?
 def valid_move?(raw_input)
