7aa22219516eac42e8522c935860ee590b4c5d3a
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-01 02:54:28 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ end
 
 #valid_move?
 def valid_move?(board, index)
-  if (index.between?(0, 8)
+  if index.between?(0, 8)
     if (board[index] == " " || board[index] == "" || board[index] == nil)
       return true
     else
