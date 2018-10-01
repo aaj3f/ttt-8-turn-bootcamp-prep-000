4bfdba440ae69e54fe94654786bb16cff50221c3
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-01 02:07:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,12 @@
 #end
 
 #display_board
-
+def display_board(board)
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
 
 #valid_move?
 
